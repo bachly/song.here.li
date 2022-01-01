6d24c2d37dd1466aeb35946d30d6871169f02208
Bach Ly <bly@cochlear.com>
2022-01-01 16:04:48 +1100

    Fix waves

--- pages/index.js
+++ pages/index.js
@@ -121,38 +121,39 @@ export default function Home() {
         <Layout>
             <div className="bg-gray-900 relative overflow-x-hidden">
                 <header className="relative" style={{ height: "100vh" }}>
-                    <div className="absolute top-0 left-0 w-full z-0 bg-image bg-no-repeat bg-cover bg-blend-multiply"
-                        style={{ backgroundImage: "url(/img/green-waves.svg)", height: "50vh" }}></div>
+                    <div className="relative top-0 left-0 w-full z-0 bg-image bg-no-repeat bg-cover bg-blend-multiply"
+                        style={{ backgroundImage: "url(/img/green-waves.svg)", height: "40vh" }}>
 
-                    <div style={{
-                        position: "absolute",
-                        width: "100%",
-                        top: "20vh",
-                        height: "150px",
-                        clipPath: "url(#clip)",
-                        transformOrigin: "left top",
-                        transform: "scale(4)",
-                        background: "linear-gradient(rgb(11 47 60) 21%, rgb(23 23 23 / 83%) 98%)"
-                    }}>
-                        <svg>
-                            <clipPath id="clip">
-                            <path d="M2.27373675e-13,48.3123102 C47.2526058,39.8757818 86.9623358,35.6575176 119.12919,35.6575176 C178.250668,35.6575176 206.827687,48.0370982 280.432303,48.0370982 C367.079105,48.0370982 376.540461,35.6575176 458.684986,41.2972131 C506.645747,44.5899961 571.277749,53.0948639 660.91839,53.0948639 C710.843933,53.0948639 849.202679,35.6575176 913.373388,35.6575176 C1071.11616,35.6575176 1092.85843,33.5132008 1277.6238,53.0948639 C1314.26691,56.9783463 1427.93201,43.4008155 1534.45719,41.2972131 C1555.56953,40.8802979 1577.41713,43.1269263 1600,48.0370982 L1600,150 L2.27373675e-13,150 L2.27373675e-13,48.3123102 Z" id="Path" stroke="#000000"></path>
-                            </clipPath>
-                        </svg>
-                    </div>
-
-                    <div className="pt-56"></div>
-
-                    <h1 className="relative z-10 px-4">
-                        <div className="text-center text-gray-100 text-sm sm:text-base md:text-lg uppercase tracking-wider font-semibold">Welcome To</div>
-                        <div className="text-gray-100 text-center text-4xl sm:text-5xl md:text-7xl">
-                            <span className="font-bold">Worship</span>
-                            <span className="font-light">Here</span>
+                        <div style={{
+                            position: "absolute",
+                            width: "100%",
+                            bottom: "60px",
+                            height: "100px",
+                            clipPath: "url(#clip)",
+                            transformOrigin: "left top",
+                            transform: "scale(3)",
+                            background: "linear-gradient(rgb(12 48 62) 41%, rgb(23 23 23 / 100%) 75%)"
+                        }}>
+                            <svg>
+                                <clipPath id="clip">
+                                    <path d="M2.27373675e-13,48.3123102 C47.2526058,39.8757818 86.9623358,35.6575176 119.12919,35.6575176 C178.250668,35.6575176 206.827687,48.0370982 280.432303,48.0370982 C367.079105,48.0370982 376.540461,35.6575176 458.684986,41.2972131 C506.645747,44.5899961 571.277749,53.0948639 660.91839,53.0948639 C710.843933,53.0948639 849.202679,35.6575176 913.373388,35.6575176 C1071.11616,35.6575176 1092.85843,33.5132008 1277.6238,53.0948639 C1314.26691,56.9783463 1427.93201,43.4008155 1534.45719,41.2972131 C1555.56953,40.8802979 1577.41713,43.1269263 1600,48.0370982 L1600,150 L2.27373675e-13,150 L2.27373675e-13,48.3123102 Z" id="Path" stroke="#000000"></path>
+                                </clipPath>
+                            </svg>
                         </div>
-                        <div className="mt-1 sm:mt-2 md:mt-3 text-center text-gray-100 text-sm md:text-xl">Personal Worship Song Books linked with AirTable</div>
-                    </h1>
 
-                    <div className="pt-96"></div>
+                        <div className="pt-56"></div>
+
+                        <h1 className="relative z-10 px-4">
+                            <div className="text-center text-gray-100 text-sm sm:text-base md:text-lg uppercase tracking-wider font-semibold">Welcome To</div>
+                            <div className="text-gray-100 text-center text-4xl sm:text-5xl md:text-7xl">
+                                <span className="font-bold">Worship</span>
+                                <span className="font-light">Here</span>
+                            </div>
+                            <div className="mt-1 sm:mt-2 md:mt-3 text-center text-gray-100 text-sm md:text-xl">Personal Worship Song Books linked with AirTable</div>
+                        </h1>
+
+                        <div className="pt-96"></div>
+                    </div>
                 </header>
             </div>
         </Layout>
